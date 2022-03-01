ba78a4b5cb747a8142d7993a215bbd8bb0e380c2
aghoghoworkpc <aghogho.akpedeye@gmail.com>
2022-03-01 12:29:43 +0000

    created use case to get all coins

---
+++ app/src/main/java/com/example/cryptocurrencycompose/domain/use_case/get_coins/GetCoinsUseCase.kt
@@ -0,0 +1,27 @@
+package com.example.cryptocurrencycompose.domain.use_case.get_coins
+
+import com.example.cryptocurrencycompose.common.Resource
+import com.example.cryptocurrencycompose.data.remote.dto.toCoin
+import com.example.cryptocurrencycompose.domain.model.Coin
+import com.example.cryptocurrencycompose.domain.repository.CoinRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import retrofit2.HttpException
+import java.io.IOException
+import javax.inject.Inject
+
+class GetCoinsUseCase @Inject constructor(
+    private val repository: CoinRepository
+) {
+    operator fun invoke(): Flow<Resource<List<Coin>>> = flow {
+        try {
+            emit(Resource.Loading())
+            val coins = repository.getCoins().map { it.toCoin() }
+            emit(Resource.Success(coins))
+        } catch (e: HttpException) {
+            emit(Resource.Error(e.localizedMessage ?: "An unexpected error occurred"))
+        } catch (e: IOException) {
+            emit(Resource.Error("Couldn't reach server, check net connection"))
+        }
+    }
+}
