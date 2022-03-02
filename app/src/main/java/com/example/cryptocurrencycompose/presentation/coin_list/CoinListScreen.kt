c46ab026cf12ce35b7e7129cc592d3165c401577
aghoghoworkpc <aghogho.akpedeye@gmail.com>
2022-03-02 20:03:02 +0000

    update coinlistscreen

--- app/src/main/java/com/example/cryptocurrencycompose/presentation/coin_list/CoinListScreen.kt
+++ app/src/main/java/com/example/cryptocurrencycompose/presentation/coin_list/CoinListScreen.kt
@@ -53,17 +53,3 @@ fun CoinListScreen(
     }
 }
 
-//Takes navController for navigation, CoinListViewModel to get an instance of state. Use hiltViewModel.
-//to automatically give us viewModel instance. In this screen we have a big box, if the api call is
-//successful, then show the list of all crypto, if it is not, show an error message at the center
-//of the screen. When you click on a coin, show progress bar at the center of the screen.
-//get the reference for the state from viewModel.state.value
-//Then get a Box with modifier. And a LazyColumn which will display List of Items. Within the
-//LazyColumn, get the items that will give us the List. Pass state.coins, and for each coin,
-//Map that to a Composable CoinListItem. CoinListItem takes coin and onItemClick as parameter.
-//When we click on a coin, we want to navigate to CoinDetailScreen. use navController.navigate()
-//For the navigate we will create a route to handle that. In Presentation package create a Screen
-//sealed class and define route for CoinListScreen and CoinDetailScreen. Back in CoinListScreen,
-//pass into the navigate the route by appending path paramter to have access to the coin we want
-//to load in CoinDetailScreen. Below the LazyColumn check if it contains an error, show a Text
-//that will contain the error.
